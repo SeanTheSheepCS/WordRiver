af97bbb13c42e4ad3bd3bacb467bf2f6352a6bdb
SeanTheSheepCS <seankenny6688@gmail.com>
2018-08-10 09:44:00 -0600

    Improved Word Class
    The word class now has a method to handle when words are typed, can break a word into the portion that has been typed and the portion that is not yet typed, and now has five new variables.

--- word.py
+++ word.py
@@ -1,6 +1,6 @@
 '''
 WordRiver typing game.
-Copyright (C) 2018 Sean Vlad C.
+Copyright (C) 2018 Sean Kenny Vlad C.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,10 +17,32 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see http://www.gnu.org/licenses/.
 '''
 
-
 class Word():
 
-    def __init__(self,value,x,y):
-        pass
+    #Note that pos_in_word is the index of the letter that should be typed, not the letter last typed
 
-    
+    def __init__(self,value,x,y):
+        self.word = value
+        self.pos_in_word = 0
+        self.fully_typed = False
+
+    def letter_typed(self,n):
+        #First we must check if the letter typed was the correct letter
+        if(n==word[pos_in_word]):
+            #The letter was correct, move on to the next one
+            pos_in_word = pos_in_word + 1
+        else:
+            #A mistake was made, reset the position back to the start
+            pos_in_word = 0
+        #Next we check if the letter we typed was the last one
+        if(pos_in_word==word.length()):
+            #We hit the end of the word
+            fully_typed = true
+
+    def portion_typed(self):
+        if((pos_in_word >= word.length()) or (pos_in_word < 0)):
+            return "Error encountered"
+
+        return (word[:pos_in_word],word[pos_in_word:])
+
+
